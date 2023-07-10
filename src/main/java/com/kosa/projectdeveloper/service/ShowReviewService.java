f3aae9b1df570f90c3809995c04f63001c0161fa
2hayoen <leehayoen0518@gmail.com>
2023-07-10 10:26:33 +0900

    "뮤지컬 후기 게시판 구현 및 수정 - 객체 생성 + 리포지토리 ++ 서비스,컨트롤러 만드는 중.."

--- src/main/java/com/kosa/projectdeveloper/service/ShowReviewService.java
+++ src/main/java/com/kosa/projectdeveloper/service/ShowReviewService.java
@@ -1,11 +1,15 @@
 package com.kosa.projectdeveloper.service;
 
+import com.kosa.projectdeveloper.domain.Book;
 import com.kosa.projectdeveloper.domain.Show;
+import com.kosa.projectdeveloper.domain.ShowReview;
 import com.kosa.projectdeveloper.dto.AddShowReviewRequest;
 import com.kosa.projectdeveloper.repository.ShowRepository;
 import com.kosa.projectdeveloper.repository.ShowReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -14,9 +18,11 @@ public class ShowReviewService {
 
     private final ShowReviewRepository showReviewRepository;
 
-    public Show save(AddShowReviewRequest request) {
+    public ShowReview save(AddShowReviewRequest request) {
         return showReviewRepository.save(request.toEntity());
     }
 
-
+    public List<ShowReview> findAllShowReview(){
+        return showReviewRepository.findAll();
+    }
 }
