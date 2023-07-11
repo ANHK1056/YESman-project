149ada908d8166e662333ba3d0bdb1deee558559
2hayoen <leehayoen0518@gmail.com>
2023-07-11 10:29:21 +0900

    "뮤지컬 후기 게시판 구현 및 수정 - CRUD 기능 확인 + 조인 다시"

--- src/main/java/com/kosa/projectdeveloper/service/ShowReviewService.java
+++ src/main/java/com/kosa/projectdeveloper/service/ShowReviewService.java
@@ -4,8 +4,10 @@ import com.kosa.projectdeveloper.domain.Book;
 import com.kosa.projectdeveloper.domain.Show;
 import com.kosa.projectdeveloper.domain.ShowReview;
 import com.kosa.projectdeveloper.dto.AddShowReviewRequest;
+import com.kosa.projectdeveloper.dto.UpdateShowReviewRequest;
 import com.kosa.projectdeveloper.repository.ShowRepository;
 import com.kosa.projectdeveloper.repository.ShowReviewRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,14 +33,21 @@ public class ShowReviewService {
     }
 
     // 리뷰 수정
-    public ShowReview updateShowReview(ShowReview showReview) {
-        return showReviewRepository.save(showReview);
+    @Transactional
+    public ShowReview update(long id, UpdateShowReviewRequest request) {
+
+        ShowReview showReview = showReviewRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        showReview.update(request.getReview_Title(), request.getReview_Content());
+
+        return showReview;
     }
 
     // 리뷰 삭제
     // Collections.singleton(review_id) 이렇게 쓰는게 맞는지 잘 모르겠어요,,
-    public void deleteShowReview(Long review_id) {
-        showReviewRepository.deleteAllById(Collections.singleton(review_id));
+    public void delete(Long review_id) {
+        showReviewRepository.deleteById(review_id);
     }
 
 
---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO reviews (review_id, review_content, review_title, book_id ) VALUES (777,  'con2', 'title2', 123 );
+INSERT INTO reviews (review_id, review_content, review_title, book_id ) VALUES (666,  'con2', 'title2', 456);
+INSERT INTO reviews (review_id, review_content, review_title, book_id ) VALUES (888,  'con3', 'title3', 789);
