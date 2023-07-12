d310b96a1eacf3ed07f6767dd783f5ec56150a90
ANHK1056 <84270386+ANHK1056@users.noreply.github.com>
2023-07-12 16:18:20 +0900

    Update AddShowReviewRequest.java

--- src/main/java/com/kosa/projectdeveloper/dto/AddShowReviewRequest.java
+++ src/main/java/com/kosa/projectdeveloper/dto/AddShowReviewRequest.java
@@ -16,36 +16,24 @@ import lombok.NoArgsConstructor;
 
 public class AddShowReviewRequest {
 
-<<<<<<< HEAD
-    private String review_Title;
-    private String review_Content;
-    private Show show;
-=======
+
     private String reviewTitle;
     private String reviewContent;
 
 //    private Show show;
->>>>>>> f5e1c8fb9d7fd2061e60d2e5934d1263ad7ba531
     private Book book;
 
 
 
     public ShowReview toEntity() {
         return ShowReview.builder()
-<<<<<<< HEAD
-                .review_Title(review_Title)
-                .review_Content(review_Content)
-//                .user(uesr)
-                .show(show)
-                .book(book)
-=======
+
                 .reviewTitle(reviewTitle)
                 .reviewContent(reviewContent)
 
 
 //                .show(show)
 //                .book(book)
->>>>>>> f5e1c8fb9d7fd2061e60d2e5934d1263ad7ba531
                 .build();
     }
 
